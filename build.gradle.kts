22e3f381e803468034da54e6d901cf135a49298e
Martmists <martmists@gmail.com>
2022-05-02 01:08:09 +0200

    version bump
    Signed-off-by: Martmists <martmists@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 allprojects {
     group = "com.martmists.kpy"
-    version = "0.1.9"
+    version = "0.1.10"
 
     tasks.withType<DependencyUpdatesTask> {
         fun isNonStable(version: String): Boolean {
