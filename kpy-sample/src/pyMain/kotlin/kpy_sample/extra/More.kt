2f5e0284983094228eab966ddb67da005c80d116
Martmists <martmists@gmail.com>
2022-05-02 01:59:09 +0200

    undo
    Signed-off-by: Martmists <martmists@gmail.com>

--- kpy-sample/src/pyMain/kotlin/kpy_sample/extra/More.kt
+++ kpy-sample/src/pyMain/kotlin/kpy_sample/extra/More.kt
@@ -1,11 +1,21 @@
 package kpy_sample.extra
 
 import kpy.annotations.PyExport
+import kpy.annotations.PyMagic
+import kpy.annotations.PyMagicMethod
+import kpy.utilities.toPython
+import kpy.wrappers.PyObjectT
+import python.PyObject_GenericGetAttr
 
 @PyExport
 class More(x: Int) : Extra(x) {
     @PyExport
     fun test() {
         println("test")
+    }
+
+    @PyMagic(PyMagicMethod.TP_GETATTRO)
+    internal fun __getattr__(name: String): PyObjectT {
+        return PyObject_GenericGetAttr(this.toPython(), name.toPython())
     }
 }
